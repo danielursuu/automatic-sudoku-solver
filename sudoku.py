8601e617206e30748a4041f61572973932a29cac
danielursuu <danielursuu@gmail.com>
2020-06-26 23:36:11 +0300

    pre-processed the cells, got rid of lines and center the digit

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,3 @@
-# sudo -_-
-
 import cv2
 import operator
 import math
@@ -7,7 +5,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 from keras.models import load_model
 from cv2 import cv2
-
 
 def show_image(img):
 
@@ -51,14 +48,11 @@ def display_rects(in_img, rects, colour=255):
         img = cv2.rectangle(img, tuple(int(x)
                                        for x in rect[0]), tuple(int(x) for x in rect[1]), colour)
     show_image(img)
-    # return img
 
 # Blur image to reduce noise obtained in thresholding algorithm
 # Binary Thresholding - makes split of either 0/1 on basis of threshold measured from entire image
 # Adaptive Thresholding - calculates threshold for each pixel based on mean value of surrounding pixels
 # Dilate image to increase thickness of lines
-
-
 def preprocess_img(img, skip_dilate=False):
     # Gaussian blur with a kernel size (height, width)
     blur = cv2.GaussianBlur(img.copy(), (9, 9), 0)
@@ -161,59 +155,34 @@ def infer_sudoku_puzzle(image, crop_rectangle):
     return crop_img()
 
 
-def infer_grid(img):  # infer 81 cells from image
-    squares = []
-    side = img.shape[:1]
+def infer_grid(img):
+	"""Infers 81 cell grid from a square image."""
+	squares = []
+	side = img.shape[:1]
+	side = side[0] / 9
+	for i in range(9):
+		for j in range(9):
+			p1 = (i * side, j * side)  # Top left corner of a bounding box
+			p2 = ((i + 1) * side, (j + 1) * side)  # Bottom right corner of bounding box
+			squares.append((p1, p2))
+	return squares
 
-    side = side[0] / 9
 
-    for i in range(9):  # get each box and append it to squares -- 9 rows, 9 cols
-        for j in range(9):
-            p1 = (i*side, j*side)  # top left corner of box
-            p2 = ((i+1)*side, (j+1)*side)  # bottom right corner of box
-            squares.append((p1, p2))
-    return squares
-
-
-def extract_number(sudoku, loaded_model):
-    sudoku = cv2.resize(sudoku, (450, 450))
-    show_image(sudoku)
+def extract_number(digits, loaded_model):
+    # sudoku = cv2.resize(sudoku, (450, 450))
 
     # split sudoku
-    grid = np.zeros([9, 9])
-    for i in range(9):
-        for j in range(9):
-            #            image = sudoku[i*50+3:(i+1)*50-3,j*50+3:(j+1)*50-3]
-            image = sudoku[i*50:(i+1)*50, j*50:(j+1)*50]
-            image=preprocess_img(image, True)
-#            filename = "images/sudoku/file_%d_%d.jpg"%(i, j)
-#            cv2.imwrite(filename, image)
-            # show_image(image)
-            if image.sum() > 25000:
-                grid[i][j] = identify_number(image, loaded_model)
-            else:
-                grid[i][j] = 0
-    return grid.astype(int)
-
-# evaluate loaded model on test data
-
-
-# def n10():
-# 	img = np.zeros((50, 50, 3), np.uint8)
-# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 	roi = img[:,:]
-# 	return roi
-
-# def getRoi(box):
-# 	padding = 5
-# 	contours, _ = cv2.findContours(box, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-# 	for cnt in contours:
-# 		[x,y,w,h] = cv2.boundingRect(cnt)
-# 		if(h > 15 and w > 5 and x > padding/2 and y > padding/2):
-# 			roi = box[y:y+h,x:x+w]
-# 			roi = cv2.resize(roi,(28,28))
-# 			return roi
-# 	return n10()
+	k = 0
+	show_image(digits[k])
+	grid = np.zeros([9, 9])
+	for i in range(9):
+		for j in range(9):
+			if digits[k].sum() > 2500:
+				grid[j][i] = identify_number(digits[k], loaded_model)
+			else:
+				grid[j][i] = 0
+			k=k+1
+	return grid.astype(int)
 
 
 def identify_number(image, loaded_model):
@@ -225,6 +194,135 @@ def identify_number(image, loaded_model):
     # print('Prediction of loaded_model: {}'.format(loaded_model_pred[0]))
     return loaded_model_pred[0]
 
+
+def cut_from_rect(img, rect):
+	"""Cuts a rectangle from an image using the top left and bottom right points."""
+	return img[int(rect[0][1]):int(rect[1][1]), int(rect[0][0]):int(rect[1][0])]
+
+
+def scale_and_centre(img, size, margin=0, background=0):
+	"""Scales and centres an image onto a new background square."""
+	h, w = img.shape[:2]
+
+	def centre_pad(length):
+		"""Handles centering for a given length that may be odd or even."""
+		if length % 2 == 0:
+			side1 = int((size - length) / 2)
+			side2 = side1
+		else:
+			side1 = int((size - length) / 2)
+			side2 = side1 + 1
+		return side1, side2
+
+	def scale(r, x):
+		return int(r * x)
+
+	if h > w:
+		t_pad = int(margin / 2)
+		b_pad = t_pad
+		ratio = (size - margin) / h
+		w, h = scale(ratio, w), scale(ratio, h)
+		l_pad, r_pad = centre_pad(w)
+	else:
+		l_pad = int(margin / 2)
+		r_pad = l_pad
+		ratio = (size - margin) / w
+		w, h = scale(ratio, w), scale(ratio, h)
+		t_pad, b_pad = centre_pad(h)
+
+	img = cv2.resize(img, (w, h))
+	img = cv2.copyMakeBorder(img, t_pad, b_pad, l_pad, r_pad, cv2.BORDER_CONSTANT, None, background)
+	return cv2.resize(img, (size, size))
+
+
+def find_largest_feature(inp_img, scan_tl=None, scan_br=None):
+	"""
+	Uses the fact the `floodFill` function returns a bounding box of the area it filled to find the biggest
+	connected pixel structure in the image. Fills this structure in white, reducing the rest to black.
+	"""
+	img = inp_img.copy()  # Copy the image, leaving the original untouched
+	height, width = img.shape[:2]
+
+	max_area = 0
+	seed_point = (None, None)
+
+	if scan_tl is None:
+		scan_tl = [0, 0]
+
+	if scan_br is None:
+		scan_br = [width, height]
+
+	# Loop through the image
+	for x in range(scan_tl[0], scan_br[0]):
+		for y in range(scan_tl[1], scan_br[1]):
+			# Only operate on light or white squares
+			if img.item(y, x) == 255 and x < width and y < height:  # Note that .item() appears to take input as y, x
+				area = cv2.floodFill(img, None, (x, y), 64)
+				if area[0] > max_area:  # Gets the maximum bound area which should be the grid
+					max_area = area[0]
+					seed_point = (x, y)
+
+	# Colour everything grey (compensates for features outside of our middle scanning range
+	for x in range(width):
+		for y in range(height):
+			if img.item(y, x) == 255 and x < width and y < height:
+				cv2.floodFill(img, None, (x, y), 64)
+
+	mask = np.zeros((height + 2, width + 2), np.uint8)  # Mask that is 2 pixels bigger than the image
+
+	# Highlight the main feature
+	if all([p is not None for p in seed_point]):
+		cv2.floodFill(img, mask, seed_point, 255)
+
+	top, bottom, left, right = height, 0, width, 0
+
+	for x in range(width):
+		for y in range(height):
+			if img.item(y, x) == 64:  # Hide anything that isn't the main feature
+				cv2.floodFill(img, mask, (x, y), 0)
+
+			# Find the bounding parameters
+			if img.item(y, x) == 255:
+				top = y if y < top else top
+				bottom = y if y > bottom else bottom
+				left = x if x < left else left
+				right = x if x > right else right
+
+	bbox = [[left, top], [right, bottom]]
+	return img, np.array(bbox, dtype='float32'), seed_point
+
+
+def extract_digit(img, rect, size):
+	"""Extracts a digit (if one exists) from a Sudoku square."""
+
+	digit = cut_from_rect(img, rect)  # Get the digit box from the whole square
+	# show_image(digit)
+
+	# Use fill feature finding to get the largest feature in middle of the box
+	# Margin used to define an area in the middle we would expect to find a pixel belonging to the digit
+	h, w = digit.shape[:2]
+	margin = int(np.mean([h, w]) / 2.5)
+	_, bbox, seed = find_largest_feature(digit, [margin, margin], [w - margin, h - margin])
+	digit = cut_from_rect(digit, bbox)
+
+	# Scale and pad the digit so that it fits a square of the digit size we're using for machine learning
+	w = bbox[1][0] - bbox[0][0]
+	h = bbox[1][1] - bbox[0][1]
+
+	# Ignore any small bounding boxes
+	if w > 0 and h > 0 and (w * h) > 100 and len(digit) > 0:
+		return scale_and_centre(digit, size, 4)
+	else:
+		return np.zeros((size, size), np.uint8)
+
+def get_digits(img, squares, size):
+	"""Extracts digits from their cells and builds an array"""
+	digits = []
+	img = preprocess_img(img.copy(), skip_dilate=True)
+	for square in squares:
+		show_image(extract_digit(img, square, size))
+		digits.append(extract_digit(img, square, size))
+	return digits
 
 def main():
     img = cv2.imread('images/sudoku1.jpg', cv2.IMREAD_GRAYSCALE)
@@ -241,7 +339,8 @@ def main():
     squares_on_sudoku = infer_grid(cropped_sudoku)
     display_rects(cropped_sudoku, squares_on_sudoku)
     # loadModel()
-    grid = extract_number(cropped_sudoku, loaded_model)
+    digits = get_digits(cropped_sudoku, squares_on_sudoku, 28)
+    grid = extract_number(digits, loaded_model)
     print(grid)
 
 
