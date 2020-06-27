8902e67c35ac1bd48ce3b8feeb35b80dd6fe1603
danielursuu <danielursuu@gmail.com>
2020-06-27 23:24:33 +0300

    refactored solver

--- sudoku-solver/solver.py
+++ sudoku-solver/solver.py
@@ -11,34 +11,34 @@ board = [
 ]
 
 
-def solve(bo):
-    find = find_empty(bo)
+def solve(board):
+    find = find_empty(board)
     if not find:
         return True
     else:
         row, col = find
 
     for i in range(1, 10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+        if valid(board, i, (row, col)):
+            board[row][col] = i
 
-            if solve(bo):
+            if solve(board):
                 return True
 
-            bo[row][col] = 0
+            board[row][col] = 0
 
     return False
 
 
-def valid(bo, num, pos):
+def valid(board, num, pos):
     # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
             return False
 
     # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
             return False
 
     # Check box
@@ -47,31 +47,31 @@ def valid(bo, num, pos):
 
     for i in range(box_y*3, box_y*3 + 3):
         for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
+            if board[i][j] == num and (i, j) != pos:
                 return False
 
     return True
 
 
-def print_board(bo):
-    for i in range(len(bo)):
+def print_board(board):
+    for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
 
-        for j in range(len(bo[0])):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
 
             if j == 8:
-                print(bo[i][j])
+                print(board[i][j])
             else:
-                print(str(bo[i][j]) + " ", end="")
+                print(str(board[i][j]) + " ", end="")
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
                 return (i, j)  # row, col
 
     return None
