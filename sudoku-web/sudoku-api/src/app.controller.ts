e53ed9a3899df71e376277257f40d80ee6681fcf
danielursuu <danielursuu@gmail.com>
2020-06-27 13:35:05 +0300

    added upload images functionality

--- sudoku-web/sudoku-api/src/app.controller.ts
+++ sudoku-web/sudoku-api/src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile, Param, Res } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
 import { AppService } from './app.service';
+import { imageFileFilter, editFileName } from './utils/image-uploading.utils';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './images',
+        filename: editFileName
+      }),
+      fileFilter: imageFileFilter
+    })
+  )
+  async uploadImage(@UploadedFile() file) {
+    const response = {
+      originalName: file.originalname,
+      fileName: file.filename
+    };
+    return response;
+  }
+
+  @Get(':path')
+  getUploadedImage(@Param('path') path, @Res() res){
+    return res.sendFile(path,{root:'./images'});
   }
 }
